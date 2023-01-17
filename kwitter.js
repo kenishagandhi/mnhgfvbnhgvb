bb24c3432ebb2c59c2065a004d2f72fd84304e1a
kenishagandhi <117497349+kenishagandhi@users.noreply.github.com>
2023-01-16 21:37:03 -0800

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,7 @@
+function addusername()
+{
+    username=document.getElementById("user_name").value;
+    localStorage.setItem("user_name", username);
+    window.location="kwitter_room.html";
+    
+}
